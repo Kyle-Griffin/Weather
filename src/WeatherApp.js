76c0ac1c311cec9735062fd461fa701d0f0ff560
Kyle-Griffin <KyleFileGriffin@gmail.com>
2019-11-21 18:26:58 +0000

    Updated with fetch api call

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -4,6 +4,7 @@ import './App.css';
 
 class WeatherApp extends React.Component {
   state = {
+    appID: '147c266bddb96d9caf03b62e816f51a3',
     currentLocation: [{
       location: 'test',
       temperature: '',
@@ -13,20 +14,52 @@ class WeatherApp extends React.Component {
   
 
   searchHandler = (event) => {
-    this.setState({
-      currentLocation: [{
-        location: event.target.locationInput.value
-      }]
-    });
     event.preventDefault();
-}
+    var locationChoice = event.target.locationInput.value;
+    // https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY
+    if (locationChoice.length > 0) { 
+      try {
+        const weatherData = getWeatherData('https://api.openweathermap.org/data/2.5/weather?q=', { location: locationChoice});
+        console.log(JSON.stringify(weatherData)); // JSON-string from `response.json()` call
+        this.setState({
+          currentLocation: [{
+            location: weatherData
+          }]
+        }); 
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      this.setState({
+        currentLocation: [{
+          location: "Please select a city" 
+        }]
+      });
+    }
+
+    async function getWeatherData(url = '', location = {}) {
+      const fullUrl = url + location.location + '&appid=147c266bddb96d9caf03b62e816f51a3';
+      const response = await fetch(fullUrl, {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(location)
+      });
+      return await response.json();
+    }
+  }
+
+  
 
   render() {
     return (
       <div className="WeatherApp">
-        <div className="weather-display"></div>
+        <div className="weather-display">
+          <p>{this.state.currentLocation[0].location}</p>
+        </div>
         <Search searchHandler={this.searchHandler.bind(this)} />
-        <p>{this.state.currentLocation[0].location}</p>
       </div>
     );
   }
